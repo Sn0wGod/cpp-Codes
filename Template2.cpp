ea03f1f63f51ca8151d22c25e96b1227a7212ac5
Yogesh Yogendra <53307155+Sn0wGod@users.noreply.github.com>
2020-10-22 14:03:25 +0530

    Up Template2

---
+++ Template2.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+#define int             long long int
+#define pb              push_back
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define mod             1000000007
+#define w(x)            int x; cin>>x; while(x--)
+using namespace std;
+
+void dfile(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+
+
+
+
+
+
+int32_t main()
+{   
+    dfile();
+    // seive();
+    w(T)
+    {
+        
+    }
+
+    return 0;   
+}
+
+
+
